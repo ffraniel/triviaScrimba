bdcde5a3352b7f6c411a30b93a17157946a18d9a
ffraniel <francis.whitehead@googlemail.com>
2022-02-24 17:31:51 +0000

    fixed gameplay, styling, vars, etc

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,6 +5,7 @@ import blueBlob from '../images/blueBlob.png';
 import yellowBlob from '../images/yellowBlob.png';
 import ShuffleArray from '../utility/ShuffleArray';
 import { nanoid } from 'nanoid'
+import shuffleArray from '../utility/ShuffleArray';
 
 export default function Game () {
 
@@ -16,45 +17,23 @@ export default function Game () {
   };
 
   const [ questions, setQuestions ] = useState([]);
+  const [ answers, setAnswers] = useState([]);
+  const [ showResults, setShowResults ] = useState(false);
+  const [ score, setScore ] = useState(null);
 
   useEffect(()=>{
     fetch('https://opentdb.com/api.php?amount=5&type=multiple')
       .then(res => res.json())
       .then(data => {
-        console.log("data: ", data.results)
-        // for each item (map)
-        // create array of answer and false answers
-        // set ID 
-        const manipulatedQuestionsArray = data.results.map(questionObj => {
-          const questionID = nanoid();
-          let updatedIncorrects = [...questionObj.incorrect_answers].map(answer => {
-            return {
-              answer: answer,
-              isSelected: false,
-              isCorrect: false,
-              id: questionID,
-              question: questionObj.question
-            }
-          });
-          let updatedCorrect = {
-            answer: questionObj.correct_answer,
-            isSelected: false,
-            isCorrect: true,
-            id: questionID,
-            question: questionObj.question
-          };
-          let options = [...updatedIncorrects, updatedCorrect];
-          // shuffle
-          let shuffledOptions = ShuffleArray(options);
-           // attach t0 each question object
-           
-           return {
-             ...questionObj,
-             gameObject: shuffledOptions
-            }
-          });
-      // set to state <<<<<<<<<<<
-      setQuestions(manipulatedQuestionsArray);
+        const arrayQuestionData = data.results.map(questionItem => {
+          let allAnswers = [...questionItem.incorrect_answers, questionItem.correct_answer];
+          let shuffledAnswers = shuffleArray(allAnswers);
+          return {
+            ...questionItem,
+            gameOptions: shuffledAnswers
+          }
+        })
+        setQuestions(arrayQuestionData)
       })
   }, []);
 
@@ -63,52 +42,80 @@ export default function Game () {
 
   }, [questions]);
 
-  function handleSelect (answerObj) {
-    let inputID = answerObj.id;
-    let inputAnswer = answerObj.answer;
-    console.log(inputID, inputAnswer);
+  function handleSelect (index, answer) {
+    setAnswers(prevAnswers => {
+      let newAnswers = [...prevAnswers];
+      newAnswers[index] = answer;     
+      return newAnswers;
+    })
+  };
 
-    setQuestions(prevQuestions => {
-      const updatedQuestions = prevQuestions.map(prevQuestion => {
-        const updatedGameObj = prevQuestion.gameObject.map(gameObjectAnswer => {
-          if (gameObjectAnswer.id === inputID) {
-            //update selected and return
-            return {
-              ...gameObjectAnswer,
-              isSelected: true
-            }
-          } else {
-            //return item unchanged
-            return gameObjectAnswer;
-          }
-        });
-        return updatedGameObj;
-
+  function handleSubmit (e) {
+    e.preventDefault();
+    if (answers.length === 5) {
+      if (answers.every(answer => answer && answer.length > 0)) {
+        let scoreCalc = answers.filter((answer, index) => {
+          return answer === questions[index].correct_answer;
+        }).length;
+        setScore(scoreCalc);
+        setShowResults(true);
+      } else {
+        alert("Please answer all the questions first");
+      }
+    } else {
+      alert("Please answer all the questions first");
+    }
+  }
+  function restart () {
+    setAnswers([]);
+    setShowResults(false);
+    setScore(null);
+    setQuestions([]);
+    fetch('https://opentdb.com/api.php?amount=5&type=multiple')
+    .then(res => res.json())
+    .then(data => {
+      const arrayQuestionData = data.results.map(questionItem => {
+        let allAnswers = [...questionItem.incorrect_answers, questionItem.correct_answer];
+        let shuffledAnswers = shuffleArray(allAnswers);
+        return {
+          ...questionItem,
+          gameOptions: shuffledAnswers
+        }
       })
-      
-      return updatedQuestions;
+      setQuestions(arrayQuestionData)
     })
   }
 
-  const questionElements = questions.map(questionObj => {
-        
+  const questionElements = questions.map((question, index) => {
     return (
       <Question 
-        gameObj={questionObj.gameObject}
-        question={questionObj.question}
+        question={question}
+        index={index}
+        answer={answers[index]}
+        setAnswers={setAnswers}
         handleSelect={handleSelect}
-        key={questionObj.id}
+        showResults={showResults}
+        id={nanoid()}
       />
       )
-    }
-  )
+  });
 
   return (
     <main className="game" style={backgroundStyleGame}>
       <div className="game-container">
         {questionElements}
       </div>
-      <button className="submit">Check Answers</button>
+      {showResults && 
+      <div className="score-container">
+        <h3>You got {score} out of 5!</h3>
+      </div>
+      }
+      {!showResults &&
+      <button className="submit" onClick={handleSubmit}>Check Answers</button>
+      }
+      {showResults && 
+      <button className="submit" onClick={restart}>Restart</button>
+      }
     </main>
   )
 };
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,24 +1,53 @@
 import './Question.css';
 
 export default function Question (props) {
+  
+  function decodeHtml(html) {
+    var txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+  }
 
-    const answerElements = props.gameObj.map(gameAnswerObj => {
-        return (
-         <li>
-            <button onClick={()=>{props.handleSelect(gameAnswerObj)}}>
-              Answer: {gameAnswerObj.answer}
-              <br/>
-              is selected {gameAnswerObj.isSelected ? "yes": "non"}
-              <br/>
-              is correct {gameAnswerObj.isCorrect ? "yes": "non"}
-              </button>
-          </li>
-        );
+  const answerElements = props.question.gameOptions.map(option => {
+
+    function passToHandler (e) {
+      e.preventDefault();
+      props.handleSelect(props.index, option)
+    }
+
+    const isSelected = props.answer ? props.answer === option ? "selected" : "" : "";
+    const isCorrect = props.answer ? 
+      props.question.correct_answer === option ?
+        "correct" : "" 
+      : "";
+    const isSelectedAndIncorrect = props.answer ? 
+      props.answer !== props.question.correct_answer ?
+       props.answer === option ? "incorrect" : ""
+      : ""
+    : "";
+
+    const btnClass = `
+      answer-btn 
+      ${isSelected} 
+      ${props.showResults && "disabled"}
+      ${props.showResults && isSelectedAndIncorrect} 
+      ${props.showResults && isCorrect}`
+
+      return (
+        <li>
+          <button 
+            onClick={passToHandler}
+            className={btnClass}
+            disabled={props.showResults ? true : false}
+          >{decodeHtml(option)}
+          </button>
+        </li>
+      );
     })
 
   return (
     <div className="question">
-      <h5 className="question-text">{props.question}</h5>
+      <h5 className="question-text">{decodeHtml(props.question.question)}</h5>
       <ul className="btn-list-container">
         {answerElements}
       </ul>
